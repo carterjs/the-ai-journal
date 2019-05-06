0bde4594463056d472f588e9ab168031bc05644a
Carter Schmalzle <cjschmal00@gmail.com>
2019-05-05 23:51:06 -0400

    added content

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBR69jYxaNJAo61e9xaGaNBZQUPwFxJAz4",
+    authDomain: "the-ai-journal.firebaseapp.com",
+    databaseURL: "https://the-ai-journal.firebaseio.com",
+    projectId: "the-ai-journal",
+    storageBucket: "the-ai-journal.appspot.com",
+    messagingSenderId: "233637150785"
+  }
 };
